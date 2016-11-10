71be37eac9725fd53ca892082abb09cf717cb820
rrogerr <rgcbll@gmail.com>
2016-11-09 18:50:59 -0600

    i love you, Nicki Minaj

--- structure_of_a_data_analysis.R
+++ structure_of_a_data_analysis.R
@@ -42,12 +42,34 @@ costFunction <- function(x,y) sum(x != (y > 0.5))
 cvError <- rep(NA, 55)
 library(boot)
 
+# tests which one of the variables in the dataset is the better 
+# predictor for an email to be SPAM
+
 for(i in 1:55){
         lmFormula <- reformulate(names(trainSpam)[i], response = "numType")
         glmFit <- glm(lmFormula, family = "binomial", data = trainSpam)
         cvError[i] <- cv.glm(trainSpam, glmFit, costFunction, 2)$delta[2]
 }
 
+# see which variable minimizes the error
 names(trainSpam)[which.min(cvError)]
+# turns out to be charDollar
 
-# predictionModel <- 
+# use the best model from the group
+predictionModel <- glm(numType ~ charDollar, family = "binomial", 
+                       data = trainSpam)
+
+# test predictor on test set
+predictionTest <- predict(predictionModel, testSpam)
+predictedSpam <- rep("nonspam", dim(testSpam)[1])
+
+# clasify as spam those with prob > 0.5
+predictedSpam[predictionModel$fitted > 0.5] <- "spam"
+
+# compare outcome of the model with actual classification
+a <- table(predictedSpam, testSpam$type)
+
+# margin of error
+err <- (a[[2]] + a[[3]])/(a[[1]] + a[[2]] + a[[3]] + a[[4]])
+
+
